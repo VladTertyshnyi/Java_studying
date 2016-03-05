8738aa19d39b1f7e61c7196eea632a0e3d1bf2f2
Maxim Gonchar <maxgonchar7@gmail.com>
2016-03-05 19:40:19 +0200

    tack changes

--- Abstract data types/Array-based/Stack.java
+++ Abstract data types/Array-based/Stack.java
@@ -1,18 +1,16 @@
-package linearDataTypes;
-
 public class Stack {
 	private static final int MAX_CAPACITY = 1000;
 	private int stackArr[];
 	private int top;
 
 	public Stack() {
-		stackArr = new int[MAX_CAPACITY];
-		top = -1;
+		this.stackArr = new int[MAX_CAPACITY];
+		this.top = -1;
 	}
 
 	public Stack(int cap) {
-		stackArr = new int[cap];
-		top = -1;
+		this.stackArr = new int[cap];
+		this.top = -1;
 	}
 
 	public int size() {
@@ -25,7 +23,6 @@ public class Stack {
 
 	public int peek() {
 		if (empty()) {
-			System.out.println("Stack is empty.");
 			return (0);
 		}
 		return (stackArr[top]);
@@ -33,7 +30,6 @@ public class Stack {
 
 	public void push(int element) {
 		if (size() == stackArr.length) {
-			System.out.println("Stack is full.");
 			return;
 		}
 		top++;
@@ -42,7 +38,6 @@ public class Stack {
 
 	public int pop() {
 		if (empty()) {
-			System.out.println("Stack is empty.");
 			return (0);
 		}
 		int temp = stackArr[top];
@@ -64,32 +59,5 @@ public class Stack {
 			System.out.printf("%d ", stackArr[i]);
 		}
 		System.out.println("");
-	}
-
-	/**
-	 * Testing function.
-	 * 
-	 * @return Capacity of the stack.
-	 */
-	public int getCapacity() {
-		return (stackArr.length);
-	}
-
-	/**
-	 * Fun function. <br>
-	 * Reverse an array with help of stack!
-	 * @return Reversed array
-	 */
-	public Integer[] reverse(Integer[] a) {
-		Stack s = new Stack(a.length);
-		Integer[] b = new Integer[a.length];
-		for (int i = 0; i < a.length; i++) // add elements to the stack. Now
-											// they are reversed
-			s.push(a[i]);
-		for (int i = 0; i < a.length; i++) // pop elements from stack to the
-											// another array - in the reversed
-											// order
-			b[i] = s.pop();
-		return (b);
 	}
 }

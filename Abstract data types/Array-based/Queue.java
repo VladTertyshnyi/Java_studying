33af232fc345447f309c21bf3b655a30a46c3df6
Maxim Gonchar <maxgonchar7@gmail.com>
2016-03-05 19:37:54 +0200

    queue changes

--- Abstract data types/Array-based/Queue.java
+++ Abstract data types/Array-based/Queue.java
@@ -1,24 +1,22 @@
-package linearDataTypes;
-
 public class Queue {
 	private static final int CAPACITY = 1000;
 	private int front;
 	private int rear;
 	private int N;
-	double[] queueArr;
+	private double[] queueArr;
 
 	public Queue() {
-		queueArr = new double[CAPACITY];
-		front = 0;
-		rear = 0;
-		N = 0;
+		this.queueArr = new double[CAPACITY];
+		this.front = 0;
+		this.rear = 0;
+		this.N = 0;
 	}
 
 	public Queue(int inputSize) {
-		queueArr = new double[inputSize];
-		front = 0;
-		rear = 0;
-		N = 0;
+		this.queueArr = new double[inputSize];
+		this.front = 0;
+		this.rear = 0;
+		this.N = 0;
 	}
 
 	public int size() {
@@ -31,8 +29,6 @@ public class Queue {
 
 	public void enqueue(double element) {
 		if (size() == queueArr.length) {
-			// System.out.println("Queue is full.");
-			// return;
 			appendElement();
 		}
 		queueArr[rear] = element;
@@ -42,21 +38,20 @@ public class Queue {
 
 	public double dequeue() {
 		if (empty()) {
-			System.out.println("Queue is empty.");
 			return (0);
 		}
 		double temp = queueArr[front];
 		front = (front + 1) % queueArr.length;
 		N--;
-		return temp;
+		return (temp);
 	}
 
 	public void appendElement() {
-		double[] queueArr_temp = new double[getCapacity() + 1];
-		System.arraycopy(queueArr, front, queueArr_temp, 0, getCapacity() - front);
-		System.arraycopy(queueArr, 0, queueArr_temp, getCapacity() - front, rear);
+		double[] queueArr_temp = new double[queueArr.length + 1];
+		System.arraycopy(queueArr, front, queueArr_temp, 0, queueArr.length - front);
+		System.arraycopy(queueArr, 0, queueArr_temp, queueArr.length - front, rear);
 		front = 0;
-		rear = getCapacity();
+		rear = queueArr.length;
 		queueArr = queueArr_temp;
 	}
 
@@ -64,7 +59,6 @@ public class Queue {
 	 * Testing function <br>
 	 * Print elements of the queue.
 	 */
-
 	public void printQueue() {
 		if (empty()) {
 			System.out.println("Queue is empty.");
@@ -74,14 +68,5 @@ public class Queue {
 			System.out.printf("%.2f ", queueArr[(front + i) % queueArr.length]);
 		}
 		System.out.println("");
-	}
-
-	/**
-	 * Testing function.
-	 * 
-	 * @return Capacity of the queue.
-	 */
-	public int getCapacity() {
-		return (queueArr.length);
 	}
 }
